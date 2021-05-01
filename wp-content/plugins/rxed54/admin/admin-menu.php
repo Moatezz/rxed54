3317f6d196a063418d1d0771350c2d367cdb3844
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-02 00:55:40 +0100

    destructering functions to seperate files in admin dir

---
+++ wp-content/plugins/rxed54/admin/admin-menu.php
@@ -0,0 +1,20 @@
+<?php // rxed54 admin menu page
+if (!defined('ABSPATH')) {
+
+    exit;
+}
+
+
+function rxed54_add_toplevel_menu()
+{
+    add_menu_page(
+        'rxed54 Settings',
+        'rxed54',
+        'manage_options',
+        'rxed54',
+        'rxed54_display_settings_page',
+        'dashicons-admin-generic',
+        30
+    );
+}
+add_action('admin_menu', 'rxed54_add_toplevel_menu');
--- wp-content/plugins/rxed54/rxed54.php
+++ wp-content/plugins/rxed54/rxed54.php
@@ -19,39 +19,24 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-function rxed54_display_settings_page()
-{
-    // Check if user is authorised for access
-
-    if (!current_user_can('manage_options')) return;
-?>
-    <div class="wrap">
-        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-        <form action="options.php" method="post">
-            <input type="text" />
-            <?php
-            //output security fields
-            settings_fields('rxed54_options');
-            //output setting sections
-            do_settings_sections('rxed54');
-            // submitting form
-            submit_button();
-            ?>
-
-        </form>
-    </div>
-<?php
+// if admin 
+if (is_admin()) {
+    //including dependencies
+    require_once plugin_dir_path(__FILE__) . 'admin/admin-menu.php';
+    require_once plugin_dir_path(__FILE__) . 'admin/settings-page.php';
 }
-function rxed54_add_toplevel_menu()
-{
-    add_menu_page(
-        'rxed54 Settings',
-        'rxed54',
-        'manage_options',
-        'rxed54',
-        'rxed54_display_settings_page',
-        'dashicons-admin-generic',
-        30
-    );
-}
-add_action('admin_menu', 'rxed54_add_toplevel_menu');
+
+
+// function rxed54_add_sublevel_menu()
+// {
+//     add_submenu_page(
+//         'rxed54.php',
+//         'rxed54 Sub menu',
+//         'rxed54 submenu',
+//         'manage_options',
+//         'rxed54',
+//         'rxed54_display_settings_page',
+//         null
+//     );
+// }
+// add_action('admin_menu', 'rxed54_add_sublevel_menu');
