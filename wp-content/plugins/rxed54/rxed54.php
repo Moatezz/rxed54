9295ab9eb58c99c11727e63e6359ccb63f0d5029
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-01 17:20:05 +0100

    Editing plugin template

--- wp-content/plugins/rxed54/rxed54.php
+++ wp-content/plugins/rxed54/rxed54.php
@@ -1,15 +1,15 @@
 /**
-* Plugin Name: Plugin Test
+* Plugin Name: rxed54
 * Plugin URI: https://example.com/plugins/the-basics/
 * Description: Just a plugin for plugin things.
 * Version: 1.0.0
 * Requires at least: 5.2
 * Requires PHP: 7.2
-* Author: Moatezz Cahbane
+* Author: Moatezz Chabane
 * Author URI: https://author.example.com/
 * License: GPL v2 or later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Text Domain: plugin-test
+* Text Domain: rxed54
 * Domain Path: /languages
 **/
 
