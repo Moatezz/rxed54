be2427c3e3a28e0e859623e25219608736656b89
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-02 12:29:47 +0100

    added settings fields cbs

--- wp-content/plugins/rxed54/admin/settings-cb.php
+++ wp-content/plugins/rxed54/admin/settings-cb.php
@@ -1,5 +1,6 @@
 <?php // Settings callbacks functions
 
+//Sections callbacks
 function rxed54_cb_section_login()
 {
     echo 'try somethings new in login section';
@@ -7,4 +8,11 @@ function rxed54_cb_section_login()
 function rxed54_cb_section_admin()
 {
     echo 'this is something of a new staff';
+}
+//Fields callbacks
+function rxed54_cb_field_text()
+{
+?>
+    <input />
+<?php
 }
