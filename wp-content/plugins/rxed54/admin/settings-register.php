4fdf8fcb2cf872367b0f41bdc4a8a901fc8ff483
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-02 12:07:46 +0100

    added settings sections

--- wp-content/plugins/rxed54/admin/settings-register.php
+++ wp-content/plugins/rxed54/admin/settings-register.php
@@ -7,5 +7,16 @@ function rxed54_register_settings()
         'rxed54_options',
         'rxed54_cb_validate_options'
     );
+    add_settings_section(
+        'rxed54_section_login',
+        'Customize Login Page',
+        'rxed54_cb_section_login',
+        'rxed54'
+    );
+    add_settings_section(
+        'rxed54_section_admin',
+        'Customize admin area',
+        'rxed54_cb_section_admin'
+    )
 }
 add_action('admin_init', 'rxed54_register_settings');
